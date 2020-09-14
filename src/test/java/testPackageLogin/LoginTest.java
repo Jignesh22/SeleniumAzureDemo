c491ba2035bc17975f0474df79d3e18874746519
Jignesh22 <jignesh.patel@radixweb.com>
2020-09-14 15:36:43 +0530

    Commit-1: Sample code created

---
+++ src/test/java/testPackageLogin/LoginTest.java
@@ -0,0 +1,22 @@
+package testPackageLogin;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void test1() {
+		System.out.println("Test Case Pass: test1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test Case Pass: test2");
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("Test Case Pass: test3");
+	}
+
+}
